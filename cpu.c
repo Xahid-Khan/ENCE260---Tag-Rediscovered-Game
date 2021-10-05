6d5e9ebd0f279ff5c94fdaf98110132f15e68c6b
Thomas Mander <tcm56@uclive.ac.nz>
2021-10-05 19:32:23 +1300

    Added random spawning and despawning of cpu enemies

--- cpu.c
+++ cpu.c
@@ -1,14 +1,53 @@
 #include "system.h"
+#include "cpu.h"
+#include "pacer.h"
+
+
+/*Spawns one enemy ensuring it is on a non-occupied LED*/
+void spawnEnemy(Position_t* enemyPositions, player_t playerPosition, int enemies)
+{
+    bool isSamePosition;
+    Position_t enemy;
+    do {
+        isSamePosition = false;
+        enemy.x = 1+rand()%5;
+        enemy.y = 1+rand()%7;
+        for(uint8_t i = 0; i < *enemies; i++) {
+            bool isSameX = (enemy.x == enemyPositions[i].x);
+            bool isSameY = (enemy.y == enemyPositions[i].y);
+            if(isSameX && isSameY) {
+                isSamePosition = true;      
+            }
+        }
+        bool isSameX = (enemy.x == playerPosition.x);
+        bool isSameY = (enemy.y == playerPosition.y);
+        if(isSameX && isSameY) {
+            isSamePosition = true;      
+        }
+    } while(isSamePosition);
+
+    enemyPositions[*enemies] = enemy;
+    *enemies += 1;
+}
+
+void deleteEnemies(Position_t* enemyPositions, uint8_t* enemies)
+{
+    *enemyPositions = NULL;
+    *enemies = 0;
+}
 
 int main (void)
 {
     system_init ();
-
-
+    uint8_t enemies = 0;
+    Position_t enemyPositions[2];
+    pacer_init(2000);
     while (1)
     {
-
-
-
+        for (uint8_t j = 0; j < 2; j++) {
+            spawnEnemy(enemyPositions, &enemies);
+        }
+        pacer_wait()
+        deleteEnemies(enemyPositions, &enemies);
     }
 }
--- cpu.h
+++ cpu.h
@@ -1,13 +1,13 @@
 #ifndef CPU_H
 #define CPU_H
 
-struct Position {
-   uint8_t x;
-   uint8_t y;
-} Position_t 
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+} Position_t;
 
-/*Moves enemy to adjacent square provided it is in LED matrix and is not occupied*/
-void moveEnemy(int* enemyPositions, int playerPosition);
+/*Removes all existing enemies*/
+void deleteEnemies(Position_t* enemyPositions, uint8_t* enemies);
 
 /*Spawns one enemy ensuring it is on a non-occupied LED*/
 void spawnEnemy(Position_t* enemyPositions, Position_t playerPosition);
